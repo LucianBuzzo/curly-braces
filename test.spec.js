68b1a2805eb9435c3f54f4dafce9f51a2f59db3e
Lucian Buzzo <lucian.buzzo@gmail.com>
2018-02-21 10:20:16 +0000

    Add spacing in test file

--- test.spec.js
+++ test.spec.js
@@ -7,24 +7,29 @@ describe('Challenge', function() {
 		assert.equal(validBraces('[]'), true);
 		assert.equal(validBraces('{}'), true);
 	});
+
 	it('should handle basic invalid braces', function() {
 		assert.equal(validBraces('(}'), false);
 		assert.equal(validBraces('[(])'), false);
 		assert.equal(validBraces('(})'), false);
 	});
+
 	it('should handle longer valid braces', function() {
 		assert.equal(validBraces('(){}[]'), true);
 		assert.equal(validBraces('([{}])'), true);
 	});
+
 	it('should handle longer invalid braces', function() {
 		assert.equal(validBraces(')(}{]['), false);
 		assert.equal(validBraces('(((({{'), false);
 	});
+
 	it('should handle tricky valid braces', function() {
 		assert.equal(validBraces('({})[({})]'), true);
 		assert.equal(validBraces('(({{[[]]}}))'), true);
 		assert.equal(validBraces('{}({})[]'), true);
 	});
+
 	it('should handle tricky invalid braces', function() {
 		assert.equal(validBraces('())({}}{()][]['), false);
 		assert.equal(validBraces('}}]]))}])'), false);
