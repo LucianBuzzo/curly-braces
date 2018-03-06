36f3cda9e72778a0fe35e321757a0323fc241a3b
Lucian Buzzo <lucian.buzzo@gmail.com>
2018-03-06 00:25:40 +0000

    Clean up and refactor eval implementation

--- src/evalFn.js
+++ src/evalFn.js
@@ -1,39 +1,20 @@
-const K = function(val) {
-	if (val.val) {
-		return this.val
-	}
-	const x = (y) => new K(y.val ? val : y)
-	x.val = val
-	return x
-}
+const K = (_ = false) =>
+	Object.assign((y) => K(y._ ? _ : y), { _ })
 
-const evalWithKContext = (prog) =>
-	function() {
-		return eval(prog)
-	}.call({ K })
-
-const s = (val) => `((a => a.val === "${val}" ? a : this.K(false))`
-const e = (val) => `(new this.K("${val}")))`
+const s = (c) => `((a => a._ === '${c}' ? a : K())`
+const e = (c) => `(K('${c}')))`
+const replacer = (x) => (m) => m === x ? s(x) : e(x)
 
 exports.parse = function validBraces(braces) {
 	braces = braces
-		.replace(/\{/g, 'xa')
-		.replace(/\}/g, 'xb')
-		.replace(/\(/g, 'ya')
-		.replace(/\)/g, 'yb')
-		.replace(/\[/g, 'za')
-		.replace(/\]/g, 'zb')
-		.replace(/ya/g, s('('))
-		.replace(/yb/g, e('('))
-		.replace(/xa/g, s('{'))
-		.replace(/xb/g, e('{'))
-		.replace(/za/g, s('['))
-		.replace(/zb/g, e('['))
+		.replace(/(\(|\))/g, replacer('('))
+		.replace(/(\{|\})/g, replacer('{'))
+		.replace(/(\[|\])/g, replacer('['))
 		.replace(/\)\)\(\(/g, ')) && ((')
 
 	try {
-		const res = evalWithKContext(braces)
-		return res.val !== false
+		const res = eval(braces)
+		return res._ !== false
 	} catch (e) {
 		return false
 	}
